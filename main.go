07d5717d8ede83c5d714f72c03529c192104995e
Ebrahim Hosseini <sehosseini@me.com>
2024-05-17 15:34:41 +0330

    add documents for functions

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
+// The line `var userProfile = map[string]string{}` is declaring a variable named `userProfile` in Go,
+// which is a map data structure. This map is defined to have keys of type `string` and values of type
+// `string`. Initially, the map is empty as it is initialized without any key-value pairs.
 var userProfile = map[string]string{}
 
+// The main function adds, prints, deletes, and updates key-value pairs in a map data structure.
 func main() {
 
 	addItem("name", "Ibrahim")
@@ -18,6 +22,7 @@ func main() {
 	printMapData()
 }
 
+// The function `printMapData` iterates over a map called `userProfile` and prints out key-value pairs.
 func printMapData() {
 	println("==================================")
 	for key, value := range userProfile {
@@ -25,10 +30,13 @@ func printMapData() {
 	}
 }
 
+// The function `deleteMapData` deletes a key-value pair from a map called `userProfile` using the
+// specified key.
 func deleteMapData(key string) {
 	delete(userProfile, key)
 }
 
+// The function `addItem` adds a key-value pair to the `userProfile` map in Go.
 func addItem(key string, value string) {
 	userProfile[key] = value
 }
